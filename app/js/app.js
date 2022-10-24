10a016f5ba9e937e5991b33ba7adf4777eaa0f63
MrBrims <magistrbrims@gmail.com>
2022-10-24 22:27:36 +0800

    added font converter and lazy load

--- app/js/app.js
+++ app/js/app.js
@@ -1,24 +1,24 @@
 import { popup } from '~/app/libs/_popup.js'
 import { burgerMenu } from '~/app/libs/_burgerMenu.js'
-import { smoothScrolling } from '~/app/libs/_smoothScrolling.js'
 import { tabs } from '~/app/libs/_tabs.js'
 import { accordion } from '~/app/libs/_accordion.js'
 import { accordionNoClose } from '~/app/libs/_accordionNoClose.js'
-import { swiperSlider } from '~/app/libs/_swiperSlider.js'
+import { swiperMudules } from '~/app/libs/_swiperMudules.js'
 import Fancybox from "@fancyapps/ui"
 import { inputPhoneCustom } from '~/app/libs/_inputPhoneCustom.js'
 import { inputNumberCastom } from '~/app/libs/_inputNumberCastom.js'
 import { inputDateCustom } from '~/app/libs/_inputDateCustom.js'
 import { selectCustom } from '~/app/libs/_selectCustom.js'
+import { lazyLoad } from '~/app/libs/_lazyLoad.js'
 import { adminIndentWp } from '~/app/libs/_adminIndentWp.js'
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  // Меню бургер
-  burgerMenu();
+  // Отложенная загрузка карты
+  lazyLoad();
 
-  // Плавная прокрутка
-  smoothScrolling();
+  // Меню бургер и плавная прокрутка
+  burgerMenu();
 
   // Попапы
   popup();
@@ -48,6 +48,6 @@ document.addEventListener('DOMContentLoaded', () => {
   selectCustom();
   
   // Слайдер
-  swiperSlider();
+  swiperMudules();
   
 })
